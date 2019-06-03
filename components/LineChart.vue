6cf0147ce2ad8af4d9570d80d30d1edac7b3f9c8
you06 <you1474600@gmail.com>
2019-06-03 13:33:37 +0800

    disable chart animation

--- components/LineChart.vue
+++ components/LineChart.vue
@@ -25,6 +25,7 @@ export default {
       options: {
         responsive: true,
         maintainAspectRatio: false,
+        animation: false,
         scales: {
           xAxes: [
             {
--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ module.exports = {
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: '#2596ec' },
 
   /*
    ** Global CSS
